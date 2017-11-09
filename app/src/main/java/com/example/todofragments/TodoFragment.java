2fe5cf71eea0dbaf090210a5cebac1cafda1bc28
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2017-11-09 22:26:54 +0000

    Todo UI Fragment

---
+++ app/src/main/java/com/example/todofragments/TodoFragment.java
@@ -0,0 +1,78 @@
+package com.example.todofragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+
+/**
+ * Created by Haleema on 09/11/2017.
+ */
+
+public class TodoFragment extends Fragment {
+
+    private Todo mTodo;
+    private EditText mEditTextTitle;
+    private Button mButtonDate;
+    private CheckBox mCheckBoxIsComplete;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mTodo = new Todo();
+        //TODO: refactor
+        mTodo.setTitle("Text title");
+        mTodo.setIsComplete(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_todo, container, false);
+
+        mEditTextTitle = (EditText) view.findViewById(R.id.todo_title);
+        mEditTextTitle.setText(mTodo.getTitle());
+        mEditTextTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This line is intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                mTodo.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //this line is intentionally left blank
+            }
+        });
+
+        mButtonDate = (Button) view.findViewById(R.id.todo_date);
+        mButtonDate.setText(mTodo.getDate().toString());
+        mButtonDate.setEnabled(false);
+
+        mCheckBoxIsComplete = (CheckBox) view.findViewById(R.id.todo_complete);
+        mCheckBoxIsComplete.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mTodo.setIsComplete(isChecked);
+
+            }
+        });
+
+        return view;
+    }
+}
