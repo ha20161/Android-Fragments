a8b2aac4a17f866f49329ba7a827d98172cf060a
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2017-11-10 18:35:05 +0000

    fragments added to activity

--- app/src/main/java/com/example/todofragments/MainActivity.java
+++ app/src/main/java/com/example/todofragments/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.todofragments;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            TodoFragment todoFragment = new TodoFragment();
+            fm.beginTransaction().add(R.id.fragment_container, todoFragment).commit();
+        }
     }
 }
