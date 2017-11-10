9befc1c808dfdec5e79a719f5f149bb1a6bffbeb
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2017-11-10 18:16:49 +0000

    Todo model class defined

---
+++ app/src/main/java/com/example/todofragments/Todo.java
@@ -0,0 +1,63 @@
+package com.example.todofragments;
+
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+/**
+ * Created by Haleema on 10/11/2017.
+ */
+
+public class Todo {
+
+    private UUID mId;
+    private String mTitle;
+    private String mDetail;
+    private Date mDate;
+    private boolean mIsComplete;
+
+    public Todo() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public void setIsComplete(boolean todoIsComplete){
+        mIsComplete = todoIsComplete;
+    }
+
+    public boolean isIsComplete(){
+        return mIsComplete;
+    }
+
+    public UUID getId(){
+        return mId;
+    }
+
+    public String getTitle(){
+        return mTitle;
+    }
+
+    public String getDetail(){
+        return mDetail;
+    }
+
+    public Date getDate(){
+        return mDate;
+    }
+
+    public void setId(UUID todoId){
+        mId = todoId;
+    }
+
+    public void setTitle(String title){
+        mTitle = title;
+    }
+
+    public void setDetail(String detail){
+        mDetail = detail;
+    }
+
+    public void setDate(Date todoDate){
+        mDate = todoDate;
+    }
+}
